037ab02afce7eb1d354947cf4e788768fc879288
姚迎迎 <yao3060@gmail.com>
2019-10-09 17:14:52 +0800

    Create config.php

---
+++ cloudamqp/config.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+define('HOST', 'rabbit-management');
+define('PORT', 5672);
+define('USER', 'rabbitmq');
+define('PASS', 'rabbitmq');
+define('VHOST', '/');
+define('AMQP_DEBUG', true);
